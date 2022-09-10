1f074c79c02c2c97d892248000d1ee328a5b43ea
IzaquielCordeiro <izaquielcordeiro@ccc.ufcg.edu.br>
2022-09-10 12:33:10 -0300

    search data function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='neuraltoolkit',
     packages=find_packages(include=['neuraltoolkit']),
-    version='0.0.1',
+    version='0.0.2',
     description='My first Python library',
     author='Me',
     license='MIT',
